8abc8f5a76dc16022e4856df38dc79af3864da76
daniyalhussain32 <daniyalhussain32@gmail.com>
2017-05-06 14:54:12 +0500

    buttons

--- application/views/clients/index.php
+++ application/views/clients/index.php
@@ -1,12 +1,3 @@
-<?php if ($this->session->userdata('advanced_search_filter')) { ?>
-    <div class="form-group"></div>
-    <div class="alert alert-info alert-dismissable">
-        <button type="button" class="close" data-dismiss="alert">&times;</button>
-        <strong><?php echo LTEXT('_message') ?> ! </strong><?php echo LTEXT('_advanced_search_is_on') ?>
-        <a class="btn btn-primary btn-small"
-           href="<?php echo base_url('admin/clients/reset_search') ?>"><?php echo LTEXT('_reset_search') ?></a>
-    </div>
-<?php } ?>
 <div class="row">
     <div class="col-sm-12">
         <div class="box box-color box-bordered">
@@ -14,65 +5,21 @@
                 <h3><?php echo LTEXT('_client_list') ?></h3>
             </div>
             <div class="box-content nopadding">
-                
-                    <table class="table table-hover table-nomargin table-bordered table-striped">
-                        <thead>
-                            <tr>
-                                <th>&nbsp;</th>
-                                <th><?php echo LTEXT('_user_id') ?><?php echo sorting('usuario_id') ?></th>
-                                <th><?php echo LTEXT('_user_name') ?><?php echo sorting('usuario_usuario') ?></th>
-                                <th><?php echo LTEXT('_company') ?><?php echo sorting('usuario_empresa') ?></th>
-                                <th><?php echo LTEXT('_email') ?><?php echo sorting('usuario_email') ?></th>
-                                <th><?php echo LTEXT('_phone') ?><?php echo sorting('usuario_telefono') ?></th>
-                                <th><?php echo LTEXT('_address') ?><?php echo sorting('usuario_direccion') ?></th>
-                                
 
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php foreach ($records as $row) { ?>
-                                <tr>
-                                    <td>
-                                        <div class="action-group">
-                                            <a
-                                                href="<?php echo base_url('admin/clients/edit_client/' . $row->usuario_id) ?>"
-                                                class="btn edit-additional-contact"> <i class="fa fa-edit"></i>
-                                            </a> <a href="<?php echo base_url('admin/clients/delete_client/' . $row->usuario_id) ?>" class="btn remove-additional-contact"> <i
-                                                    class="fa fa-times"></i>
-                                            </a>
-                                        </div>
-                                    </td>
-                                    <td><?php echo $row->usuario_id; ?></td>
-                                    <td><?php echo $row->usuario_usuario; ?></td>
-                                    <td><?php echo $row->usuario_empresa; ?></td>
-                                    <td><?php echo $row->usuario_email; ?></td>
-                                    <td><?php echo $row->usuario_telefono; ?></td>
-                                    <td><?php echo $row->usuario_direccion; ?></td>
-                                    
-                                </tr>  
-                            <?php } ?>
-                        </tbody>
-                    </table>
-                <form method="post" id="form-sort">
-                    <input type="hidden" value="" id="sort-order" name="sort-order" />
-                    <input type="hidden" value="" id="sort-field" name="sort-field" />
-               </form>
-                <div class="pull-right">
-                    <?php echo $this->pagination->create_links(); ?>
-                </div>
-
+                <table id="mytabless" class="table table-hover table-nomargin table-bordered dataTable-column_filter" data-column_filter_types="text,text,text,text,text,text,null">
+                    <thead>
+                        <tr>
+                            <th><?php echo LTEXT('_user_id') ?></th>
+                            <th><?php echo LTEXT('_user_name') ?></th>
+                            <th><?php echo LTEXT('_company') ?></th>
+                            <th><?php echo LTEXT('_email') ?></th>
+                            <th><?php echo LTEXT('_phone') ?></th>
+                            <th><?php echo LTEXT('_address') ?></th>
+                            <th>&nbsp;</th>
+                        </tr>
+                    </thead>
+                </table>
             </div>
         </div>
     </div>
 </div>
-<script>
-    // A $( document ).ready() block.
-    $(document).ready(function () {
-        
-        $(".fa-caret-up, .fa-caret-down").click( function () {
-            $('#sort-field').val($(this).data('field'));
-            $('#sort-order').val($(this).data('sort'));
-            $('#form-sort').submit();
-        });
-    });
-</script>
--- assets/js/products_view.js
+++ assets/js/products_view.js
@@ -27,7 +27,42 @@ $(document).ready(function () {
             {"orderSequence": ["desc", "asc"]},
             {"searchable": false, orderable: false, targets: -1},
         ],
+        "columnDefs": [
+            {
+                "render": function (data) {
+                    return '<button data-id="' + data + '" class="btn btn-xs btn-default detail">Details</button>';
+                },
+                "targets": 10
+            }
+        ]
     };
     // this function is present in myscripts.js file . renders advanced datatables with serverside processing
     render_datatable(my_options);
+
+    $(document).on('click', '.detail', function () {
+        id = $(this).attr('data-id');
+        $.ajax({
+            url: base_url + "products/get_product/"+id,
+            type: "GET",
+            // async: false,
+            dataType: "json",
+            success: function (data) {
+                $('#modal-1').modal(true);
+                $('#code').html(data.Code);
+                $('#prod_grp').html(data.Prod_Grp);
+                $('#rim').html(data.Rim);
+                $('#ptrn_family').html(data.Pattern_Family);
+                $('#type').html(data.Type);
+                $('#tt').html(data.Tubed_Tubeless);
+                $('#stock').html(data.Stock);
+                $('#source').html(data.Source);
+                $('#size').html(data.Size);
+                $('#Brand').html(data.Brand);
+                $('#net_price').html(data.Net_Price);
+                $('#Price_FOB').html(data.CCT_Price_FOB_2004_Rounded);
+                
+                
+            }
+        });
+    });
 });
